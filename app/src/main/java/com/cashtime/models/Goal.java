51a4c6e7a0e86bd002ff3e87fa01dffbc1e63697
probuse <nprobuse@gmail.com>
2017-08-04 11:28:51 +0300

    Initial commit

--- app/src/main/java/com/cashtime/models/Goal.java
+++ app/src/main/java/com/cashtime/models/Goal.java
@@ -1,8 +1,65 @@
 package com.cashtime.models;
 
 /**
- * Created by probuse on 8/4/17.
+ * Created by cashtime on 8/4/17.
  */
 
 public class Goal {
+    private long id;
+    private String name;
+    private int amount;
+    private String startDate;
+    private String endDate;
+    private User user;
+
+    public Goal() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }

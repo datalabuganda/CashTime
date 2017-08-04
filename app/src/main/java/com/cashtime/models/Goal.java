60fb612bc1a14bfaedba579a1d1cc5bad58968a8
probuse <nprobuse@gmail.com>
2017-08-04 11:25:05 +0300

    changed Person Model to User Model

---
+++ app/src/main/java/com/cashtime/models/Goal.java
@@ -0,0 +1,8 @@
+package com.cashtime.models;
+
+/**
+ * Created by probuse on 8/4/17.
+ */
+
+public class Goal {
+}
---
+++ app/src/main/java/com/cashtime/models/User.java
@@ -0,0 +1,79 @@
+package com.cashtime.models;
+
+/**
+ * Created by cashtime on 7/18/17.
+ */
+
+public class User {
+    long id, points;
+    int household, age;
+    String sex, educationLevel, nationality, phoneNumber;
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getPoints() {
+        return points;
+    }
+
+    public void setPoints(long points) {
+        this.points = points;
+    }
+
+    public int getHousehold() {
+        return household;
+    }
+
+    public void setHousehold(int household) {
+        this.household = household;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEducationLevel() {
+        return educationLevel;
+    }
+
+    public void setEducationLevel(String educationLevel) {
+        this.educationLevel = educationLevel;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}

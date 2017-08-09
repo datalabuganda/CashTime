06cbee3ce1b25c966badaba0e5e041d5b897f226
probuse <nprobuse@gmail.com>
2017-08-09 22:44:42 +0300

    award 2 points for spending on home needs

--- app/src/main/java/com/example/eq62roket/CashTime/activities/HomeneedsActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/HomeneedsActivity.java
@@ -10,12 +10,15 @@ import android.widget.Toast;
 
 import com.example.eq62roket.CashTime.R;
 import com.example.eq62roket.CashTime.helper.SQLiteHelper;
+import com.example.eq62roket.CashTime.helper.UserCrud;
+import com.example.eq62roket.CashTime.models.User;
 
 public class HomeneedsActivity extends AppCompatActivity {
 
     SQLiteHelper myHelper;
     EditText edtHomeneeds;
     Button btnHomeneeds;
+    UserCrud userCrud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,9 @@ public class HomeneedsActivity extends AppCompatActivity {
         edtHomeneeds = (EditText) findViewById(R.id.amtHomeneeds);
         btnHomeneeds = (Button) findViewById(R.id.btnHomeneeds);
         myHelper = new SQLiteHelper(this);
+
+        userCrud = new UserCrud(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         AddHomeneeds();
@@ -36,14 +42,27 @@ public class HomeneedsActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        int yVal = Integer.parseInt(String.valueOf(edtHomeneeds.getText()));
-                        boolean isInseted = myHelper.insertHomeneeds(yVal);
-                        if (isInseted = true)
-                            Toast.makeText(HomeneedsActivity.this, "Your home needs costs have been stored", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(HomeneedsActivity.this, "Your home needs costs where not stored", Toast.LENGTH_LONG).show();
-                        Intent Homeneedsintent = new Intent(HomeneedsActivity.this, ExpenditureActivity.class);
-                        HomeneedsActivity.this.startActivity(Homeneedsintent);
+                        if (!edtHomeneeds.getText().toString().equals("")) {
+                            int yVal = Integer.parseInt(String.valueOf(edtHomeneeds.getText()));
+                            boolean isInseted = myHelper.insertHomeneeds(yVal);
+                            if (isInseted) {
+                                // if user spends on any expense, award them 2 points
+                                User user = userCrud.getLastUserInserted();
+                                user.setPoints(2);
+                                userCrud.updateUser(user);
+
+                                Toast.makeText(HomeneedsActivity.this, "Your home needs costs have been stored", Toast.LENGTH_LONG).show();
+                                Intent Homeneedsintent = new Intent(HomeneedsActivity.this, ExpenditureActivity.class);
+                                HomeneedsActivity.this.startActivity(Homeneedsintent);
+                            }
+                            else {
+                                Toast.makeText(HomeneedsActivity.this, "Your home needs costs where not stored", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(HomeneedsActivity.this, "Please input amount before submitting", Toast.LENGTH_LONG).show();
+                        }
+
                     }
 
                 }

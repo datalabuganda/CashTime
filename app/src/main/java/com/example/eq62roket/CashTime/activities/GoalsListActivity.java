afdc2c5ce759107a1c4a8f5883a036cc2f88d1ed
probuse <nprobuse@gmail.com>
2017-08-09 12:51:24 +0300

    added functionality to stop creation of a new goal if there is an on going goal

--- app/src/main/java/com/example/eq62roket/CashTime/activities/GoalsListActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/GoalsListActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import com.example.eq62roket.CashTime.R;
 import com.example.eq62roket.CashTime.adapters.GoalListAdapter;
 import com.example.eq62roket.CashTime.helper.GoalCrud;
+import com.example.eq62roket.CashTime.helper.SQLiteHelper;
 import com.example.eq62roket.CashTime.models.Goal;
 
 import java.util.ArrayList;
@@ -21,8 +23,7 @@ public class GoalsListActivity extends AppCompatActivity {
     private static final String TAG = "GoalActivity";
 
     private GoalCrud goalCrud;
-
-    SharedPreferences preferences;
+    SQLiteHelper sqliteHelper;
 
     ListView listView;
 
@@ -34,6 +35,11 @@ public class GoalsListActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
         goalCrud = new GoalCrud(this);
+        sqliteHelper = new SQLiteHelper(this);
+
+        // get amount saved on goal and goal amount.
+        final int goal_amount = goalCrud.getLastInsertedGoal().getAmount();
+        final int goal_saving = sqliteHelper.addAllSavings();
 
         ArrayList<Goal> goalArrayList = new ArrayList<>();
         goalArrayList = goalCrud.getAllGoals();
@@ -41,17 +47,16 @@ public class GoalsListActivity extends AppCompatActivity {
         GoalListAdapter goalListAdapter = new GoalListAdapter(this, R.layout.goal_list_adapter, goalArrayList);
         listView.setAdapter(goalListAdapter);
 
-        preferences = getSharedPreferences(GoalDetailActivity.PREF_NAME, 0);
-        final boolean goalOngoing = preferences.getBoolean("goalOngoing", false);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (goalOngoing){
+                if (goal_amount > goal_saving){
                     Toast.makeText(GoalsListActivity.this,
                             "You must complete the current goal to set another goal.",
                             Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "alive");
                 }
                 else {
                     Intent intent = new Intent(GoalsListActivity.this, AddGoalActivity.class);

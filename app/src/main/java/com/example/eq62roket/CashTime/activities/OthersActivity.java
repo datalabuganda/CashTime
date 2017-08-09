bff60db43373e9ff3361a51651b33f03ca5cf745
probuse <nprobuse@gmail.com>
2017-08-09 22:56:10 +0300

    addd functionality to award user 2 points for adding any expenditure in others

--- app/src/main/java/com/example/eq62roket/CashTime/activities/OthersActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/OthersActivity.java
@@ -10,12 +10,15 @@ import android.widget.Toast;
 
 import com.example.eq62roket.CashTime.R;
 import com.example.eq62roket.CashTime.helper.SQLiteHelper;
+import com.example.eq62roket.CashTime.helper.UserCrud;
+import com.example.eq62roket.CashTime.models.User;
 
 public class OthersActivity extends AppCompatActivity {
 
     SQLiteHelper myHelper;
     EditText edtOthers;
     Button btnOthers;
+    UserCrud userCrud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,8 @@ public class OthersActivity extends AppCompatActivity {
         btnOthers = (Button) findViewById(R.id.btnOthers);
         myHelper = new SQLiteHelper(this);
 
+        userCrud = new UserCrud(this);
+
         AddOthers();
     }
 
@@ -36,14 +41,26 @@ public class OthersActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        int yVal = Integer.parseInt(String.valueOf(edtOthers.getText()));
-                        boolean isInseted = myHelper.insertOthers(yVal);
-                        if (isInseted = true)
-                            Toast.makeText(OthersActivity.this, "Your costs of other items have been stored", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(OthersActivity.this, "Other costs have not been stored", Toast.LENGTH_LONG).show();
-                        Intent Othersintent = new Intent(OthersActivity.this, ExpenditureActivity.class);
-                        OthersActivity.this.startActivity(Othersintent);
+                        if (!edtOthers.getText().toString().equals("")) {
+                            int yVal = Integer.parseInt(String.valueOf(edtOthers.getText()));
+                            boolean isInseted = myHelper.insertOthers(yVal);
+                            if (isInseted) {
+                                // if user spends on any expense, award them 2 points
+                                User user = userCrud.getLastUserInserted();
+                                user.setPoints(2);
+                                userCrud.updateUser(user);
+
+                                Intent Othersintent = new Intent(OthersActivity.this, ExpenditureActivity.class);
+                                OthersActivity.this.startActivity(Othersintent);
+                                Toast.makeText(OthersActivity.this, "Your costs of other items have been stored", Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(OthersActivity.this, "Other costs have not been stored", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(OthersActivity.this, "Please input amount before submitting", Toast.LENGTH_LONG).show();
+                        }
                     }
 
                 }

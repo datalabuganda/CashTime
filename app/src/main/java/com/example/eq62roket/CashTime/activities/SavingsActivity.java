3c59567bcefe6e7fc6b455d825e4cb9877606ad0
probuse <nprobuse@gmail.com>
2017-08-09 16:40:10 +0300

    give user a toast message if they hav not added anything in the edit text field

--- app/src/main/java/com/example/eq62roket/CashTime/activities/SavingsActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/SavingsActivity.java
@@ -3,6 +3,7 @@ package com.example.eq62roket.CashTime.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,10 +11,15 @@ import android.widget.Toast;
 
 import com.example.eq62roket.CashTime.R;
 import com.example.eq62roket.CashTime.helper.SQLiteHelper;
+import com.example.eq62roket.CashTime.helper.UserCrud;
+import com.example.eq62roket.CashTime.models.User;
 
 public class SavingsActivity extends AppCompatActivity {
 
+    private static final String TAG = "SavingsActivity";
+
     SQLiteHelper myHelper;
+    UserCrud userCrud;
     EditText edtSavings;
     Button btnSavings;
 
@@ -26,6 +32,8 @@ public class SavingsActivity extends AppCompatActivity {
         btnSavings = (Button) findViewById(R.id.btnSavings);
         myHelper = new SQLiteHelper(this);
 
+        userCrud = new UserCrud(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         AddSavings();
@@ -36,14 +44,33 @@ public class SavingsActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        int yVal = Integer.parseInt(String.valueOf(edtSavings.getText()));
-                        boolean isInseted = myHelper.insertSavings(yVal);
-                        if (isInseted = true)
-                            Toast.makeText(SavingsActivity.this, "Your savings have been stored", Toast.LENGTH_LONG).show();
-                        else
-                            Toast.makeText(SavingsActivity.this, "Your savings have not been stored", Toast.LENGTH_LONG).show();
-                        Intent Savingsintent = new Intent(SavingsActivity.this, ExpenditureActivity.class);
-                        SavingsActivity.this.startActivity(Savingsintent);
+
+                        if (  !edtSavings.getText().toString().equals("")  ){
+                            Log.d(TAG, "am here---" + edtSavings.getText()+"---");
+
+                            int yVal = Integer.parseInt(String.valueOf(edtSavings.getText()));
+
+                            boolean isInseted = myHelper.insertSavings(yVal);
+
+
+                            if (isInseted) {
+
+                                // if user creates goal for the first time, award them 3 points
+                                User user = userCrud.getLastUserInserted();
+                                user.setPoints(2);
+                                userCrud.updateUser(user);
+
+                                Toast.makeText(SavingsActivity.this, "Your savings have been stored", Toast.LENGTH_LONG).show();
+                                Intent Savingsintent = new Intent(SavingsActivity.this, ExpenditureActivity.class);
+                                SavingsActivity.this.startActivity(Savingsintent);
+                            }
+                            else {
+                                Toast.makeText(SavingsActivity.this, "Your savings have not been stored", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(SavingsActivity.this, "Please input amount before submitting", Toast.LENGTH_LONG).show();
+                        }
                     }
 
                 }

e42470cf5e9b9805cd5eca316e2b1adac2b8d7d8
eq62Roket <otimtony>
2017-08-22 19:34:28 +0300

    Replaced reportActivity with CashTime to fix the R error we had yesterday night

---
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/IncomeTabbedAnalysis.java
@@ -0,0 +1,8 @@
+package com.example.eq62roket.CashTime.activities;
+
+/**
+ * Created by eq62roket on 8/22/17.
+ */
+
+public class IncomeTabbedAnalysis {
+}
--- app/src/main/java/com/example/eq62roket/CashTime/activities/UpdateOtherIncomesActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/UpdateOtherIncomesActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.eq62roket.CashTime.R;
 
-public class UpdateSalaryActivity extends AppCompatActivity {
+public class UpdateOtherIncomesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_salary2);
+        setContentView(R.layout.activity_update_other_incomes);
     }
 }

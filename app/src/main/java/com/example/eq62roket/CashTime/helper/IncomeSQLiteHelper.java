1be0d68ce3a3d48c13be51ea9964c7046e213a09
eq62Roket <otimtony>
2017-08-22 19:38:36 +0300

    Added methods to update, and get which where used to get listviews of each of the income sources and update each of the items in the listView

--- app/src/main/java/com/example/eq62roket/CashTime/helper/IncomeSQLiteHelper.java
+++ app/src/main/java/com/example/eq62roket/CashTime/helper/IncomeSQLiteHelper.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.eq62roket.CashTime.models.Goal;
+import com.example.eq62roket.CashTime.models.Income;
 
 /**
  * Created by eq62roket on 8/3/17.
@@ -20,6 +24,7 @@ public class IncomeSQLiteHelper extends SQLiteOpenHelper {
     public static final String COL_4 = "LOAN";
     public static final String COL_6 = "INVESTMENT";
     public static final String COL_8 = "OTHERS";
+    private static final String TAG = "IncomeSQLiteHelper";
 
     public IncomeSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -47,6 +52,30 @@ public class IncomeSQLiteHelper extends SQLiteOpenHelper {
         else
             return true;
     }
+    public Cursor getSalary(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = " SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return  data;
+    }
+
+    public Cursor getSalaryID(String salary){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_1 + " FROM " + TABLE_NAME + " WHERE " + COL_3 + " = '" + salary + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void updateSalary(String newSalary, int id, String oldSalary){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL_3 + " = '" + newSalary + "' WHERE " + COL_1 + " = '" + id + "'" + " AND " + COL_3 + " = '" + oldSalary + "'";
+
+        Log.d(TAG, "updateSalary: query: " + query);
+        Log.d(TAG, "updateSalary: Setting salary to " + newSalary);
+        db.execSQL(query);
+    }
+
+
 
     public boolean insertLoan(int loan){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -59,6 +88,30 @@ public class IncomeSQLiteHelper extends SQLiteOpenHelper {
             return true;
     }
 
+    public Cursor getLoan(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return  data;
+    }
+
+    public Cursor getLoanID(String loan){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_1 + " FROM " + TABLE_NAME + " WHERE " + COL_4 + " = '" + loan + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void updateLoan(String newLoan, int id, String oldLoan){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL_4 + " = '" + newLoan + "' WHERE " + COL_1 + " = '" + id + "'" + " AND " + COL_4 + " = '" + oldLoan + "'";
+
+        Log.d(TAG, "updateLoan: query: " + query);
+        Log.d(TAG, "updateLoan: Setting loan to " + newLoan);
+        db.execSQL(query);
+    }
+
+
     public boolean insertInvestment(int investment){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues investmentValues = new ContentValues();
@@ -69,6 +122,30 @@ public class IncomeSQLiteHelper extends SQLiteOpenHelper {
         else
             return true;
     }
+
+    public Cursor getInvestment(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_6 + " FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return  data;
+    }
+
+    public Cursor getInvestmentID(String investment){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_1 + " FROM " + TABLE_NAME + " WHERE " + COL_6 + " = '" + investment + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void updateInvestment(String newInvestment, int id, String oldInvestment){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL_6 + " = '" + newInvestment + "' WHERE " + COL_1 + " = '" + id + "'" + " AND " + COL_6 + " = '" + oldInvestment + "'";
+
+        Log.d(TAG, "updateInvestment: query: " + query);
+        Log.d(TAG, "updateInvestment: Setting salary to " + newInvestment);
+        db.execSQL(query);
+    }
+
 
 
     public boolean insertOthers(int others){
@@ -81,6 +158,30 @@ public class IncomeSQLiteHelper extends SQLiteOpenHelper {
         else
             return true;
     }
+
+    public Cursor getOthers(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        Cursor data = db.rawQuery(query, null);
+        return  data;
+    }
+
+    public Cursor getOthersID(String others){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT " + COL_1 + " FROM " + TABLE_NAME + " WHERE " + COL_8 + " = '" + others + "'";
+        Cursor data = db.rawQuery(query, null);
+        return data;
+    }
+
+    public void updateOthers(String newOthers, int id, String oldOthers){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "UPDATE " + TABLE_NAME + " SET " + COL_8 + " = '" + newOthers + "' WHERE " + COL_1 + " = '" + id + "'" + " AND " + COL_8 + " = '" + oldOthers + "'";
+
+        Log.d(TAG, "updateOthers: query: " + query);
+        Log.d(TAG, "updateOthers: Setting others to " + newOthers);
+        db.execSQL(query);
+    }
+
 
 
     public int addAllLoan(){

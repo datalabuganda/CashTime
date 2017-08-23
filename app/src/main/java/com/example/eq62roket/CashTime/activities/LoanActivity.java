b9491b210ee81c0f24d410d1beb0ea911d7c1cf6
eq62Roket <otimtony>
2017-08-23 18:06:52 +0300

    Included the listView from xml that wasnt included earlier and added a method to populate the listView

--- app/src/main/java/com/example/eq62roket/CashTime/activities/LoanActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/LoanActivity.java
@@ -1,11 +1,17 @@
 package com.example.eq62roket.CashTime.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.eq62roket.CashTime.R;
@@ -14,13 +20,17 @@ import com.example.eq62roket.CashTime.helper.UserCrud;
 
 import com.example.eq62roket.CashTime.models.User;
 
+import java.util.ArrayList;
+
 
 public class LoanActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoanActivity";
     EditText edtLoans;
     Button btnLoans;
     IncomeSQLiteHelper myHelper;
     UserCrud userCrud;
+    ListView LoanListVIew;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +41,11 @@ public class LoanActivity extends AppCompatActivity {
 
         btnLoans = (Button) findViewById(R.id.btnUpdateLoans);
         edtLoans = (EditText) findViewById(R.id.edtUpdateLoans);
+        LoanListVIew = (ListView) findViewById(R.id.LoanListView);
 
         userCrud = new UserCrud(this);
         AddLoan();
+        populateListView();
     }
 
 
@@ -68,4 +80,43 @@ public class LoanActivity extends AppCompatActivity {
                 }
         );
     }
+
+    private void populateListView(){
+        Log.d(TAG, "populateListView: Displayng data in the listView");
+
+        Cursor data = myHelper.getLoan();
+        Log.d(TAG, "here is data: " + data);
+        ArrayList<String> listData = new ArrayList<>();
+        while (data.moveToNext()){
+            listData.add(data.getString(data.getColumnIndex(myHelper.COL_4)));
+        }
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        LoanListVIew.setAdapter(adapter);
+
+        LoanListVIew.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String loan = adapterView.getItemAtPosition(i).toString();
+                Log.d(TAG, "onItemClick: You clicked on" + loan);
+
+                Cursor data = myHelper.getLoanID(loan);
+                int loanID = -1;
+                while (data.moveToNext()){
+                    loanID = data.getInt(0);
+
+                }
+                if (loanID > -1){
+                    Intent editLoanIntent = new Intent(LoanActivity.this, UpdateLoanActivity.class);
+                    editLoanIntent.putExtra("id", loanID);
+                    editLoanIntent.putExtra("loan", loan);
+                    Log.d(TAG, "almost through: " + loan);
+                    startActivity(editLoanIntent);
+
+                }else {
+
+                }
+            }
+        });
+    }
+
 }

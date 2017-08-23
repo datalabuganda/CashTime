db37eab0f335867d06ccbbf79d7bcc0742910d7a
eq62Roket <otimtony>
2017-08-23 18:01:32 +0300

    Wrote code to display the listView of investments amounts

--- app/src/main/java/com/example/eq62roket/CashTime/activities/InvestmentActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/InvestmentActivity.java
@@ -89,10 +89,7 @@ public class InvestmentActivity extends AppCompatActivity {
         Log.d(TAG, "here is data: " + data);
         ArrayList<String> listData = new ArrayList<>();
         while (data.moveToNext()){
-            listData.add(data.getString(0));
-            listData.add(data.getString(2));
-            listData.add(data.getString(3));
-            listData.add(data.getString(4));
+            listData.add(data.getString(data.getColumnIndex(myHelper.COL_6)));
         }
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         investmentListView.setAdapter(adapter);

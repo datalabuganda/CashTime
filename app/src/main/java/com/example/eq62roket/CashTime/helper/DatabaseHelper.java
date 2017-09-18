f5affb6dd4ca5e5642c8ccbefee22b5845be7ce0
eq62Roket <otimtony>
2017-09-18 13:09:31 +0300

    Updated the database version

--- app/src/main/java/com/example/eq62roket/CashTime/helper/DatabaseHelper.java
+++ app/src/main/java/com/example/eq62roket/CashTime/helper/DatabaseHelper.java
@@ -14,7 +14,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TAG = "DatabaseHelper";
 
     private static final String DATABASE_NAME = "cashTime.db";
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 6;
 
 
     // Columns of User table

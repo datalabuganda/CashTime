a7f563f58f12dbcb805581f0687bf9c6bcaaef12
eq62Roket <otimtony>
2017-10-02 09:36:02 +0300

    Added a period dropdown

--- app/src/main/java/com/example/eq62roket/CashTime/activities/LoanActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/LoanActivity.java
@@ -1,5 +1,7 @@
 package com.example.eq62roket.CashTime.activities;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -26,6 +28,7 @@ import com.example.eq62roket.CashTime.helper.UserCrud;
 import com.example.eq62roket.CashTime.models.Goal;
 import com.example.eq62roket.CashTime.models.Income;
 import com.example.eq62roket.CashTime.models.User;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import java.util.ArrayList;
 
@@ -35,13 +38,16 @@ import static com.example.eq62roket.CashTime.R.id.parent;
 public class LoanActivity extends AppCompatActivity {
 
     private static final String TAG = "LoanActivity";
-    EditText edtLoans;
+    EditText edtLoans,  loanspinner;
     Button btnLoans;
     IncomeCrud incomeCrud;
     UserCrud userCrud;
     ListView LoanListVIew;
     LoanAdapter loanAdapter;
 
+
+//    public static String[] loan = {"Daily", "Weekly", "Monthly"};
+    public static String[] loan = {"Daily", "Weekly", "Monthly"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +58,17 @@ public class LoanActivity extends AppCompatActivity {
         btnLoans = (Button) findViewById(R.id.btnUpdateLoans);
         edtLoans = (EditText) findViewById(R.id.edtUpdateLoans);
         LoanListVIew = (ListView) findViewById(R.id.LoanListView);
+        loanspinner = (EditText) findViewById(R.id.loanSpinner);
+
+        // adapter for gender
+        ArrayAdapter<String> loanSpinnerAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.support_simple_spinner_dropdown_item,
+                loan
+        );
+
+        MaterialBetterSpinner materialLoanSpinner = (MaterialBetterSpinner) findViewById(R.id.loanSpinner);
+        materialLoanSpinner.setAdapter(loanSpinnerAdapter);
 
         ArrayList<Income> loanArrayList = new ArrayList<>();
         loanArrayList = incomeCrud.getAllLoan();
@@ -72,9 +89,10 @@ public class LoanActivity extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (!edtLoans.getText().toString().equals("")) {
-                            int yVal = Integer.parseInt(String.valueOf(edtLoans.getText()));
-                            boolean isInseted = incomeCrud.insertLoan(yVal);
+//                        int yVal = Integer.parseInt(String.valueOf(edtLoans.getText()));
+                        if (!edtLoans.getText().toString().equals("") && !loanspinner.getText().toString().equals("")) {
+//                            int yVal = Integer.parseInt(String.valueOf(edtLoans.getText()));
+                            boolean isInseted = incomeCrud.insertLoan(Integer.parseInt(edtLoans.getText().toString()), loanspinner.getText().toString());
                             if (isInseted) {
                                 // if user adds income, award them 2 points
                                 User user = userCrud.getLastUserInserted();
@@ -83,6 +101,7 @@ public class LoanActivity extends AppCompatActivity {
                                 userCrud.updateUser(user);
 
                                 Toast.makeText(LoanActivity.this, "Your income has been added", Toast.LENGTH_LONG).show();
+
                                 Intent Loanintent = new Intent(LoanActivity.this, IncomeActivity.class);
                                 LoanActivity.this.startActivity(Loanintent);
                                 finish();
@@ -91,7 +110,7 @@ public class LoanActivity extends AppCompatActivity {
                             }
                         }
                         else{
-                            Toast.makeText(LoanActivity.this, "Please input amount before submitting", Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoanActivity.this, "Please input amount and period before submitting", Toast.LENGTH_LONG).show();
                         }
 
                     }

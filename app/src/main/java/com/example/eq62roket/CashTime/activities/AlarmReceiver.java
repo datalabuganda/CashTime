e5bdd226b3136c6268511640ad1204eb94da2349
eq62Roket <otimtony>
2017-10-02 09:37:02 +0300

    Added a alarm broadacast receive in manifest

--- app/src/main/java/com/example/eq62roket/CashTime/activities/AlarmReceiver.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/AlarmReceiver.java
@@ -1,8 +1,17 @@
 package com.example.eq62roket.CashTime.activities;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 /**
  * Created by eq62roket on 10/1/17.
  */
 
-public class AlarmReceiver {
+public class AlarmReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"ALARM....", Toast.LENGTH_LONG).show();
+    }
 }

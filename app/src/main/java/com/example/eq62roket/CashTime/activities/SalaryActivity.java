31dab70972ed7fd1950fd6fd5a5f7073f0ee6eb5
eq62Roket <otimtony>
2017-10-05 12:15:33 +0300

    Created a method to deactivate the button when the current date is less the the period of the amount set by the application user

--- app/src/main/java/com/example/eq62roket/CashTime/activities/SalaryActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/SalaryActivity.java
@@ -2,6 +2,7 @@ package com.example.eq62roket.CashTime.activities;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,7 +25,16 @@ import com.example.eq62roket.CashTime.models.Income;
 import com.example.eq62roket.CashTime.models.User;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Map;
+
+import static com.example.eq62roket.CashTime.helper.DatabaseHelper.COLUMN_INCOME_SALARY;
 
 
 public class SalaryActivity extends AppCompatActivity {
@@ -36,7 +46,8 @@ public class SalaryActivity extends AppCompatActivity {
     ListView SalaryListVIew;
     SalaryAdapter salaryAdapter;
     Cursor c;
-    public static String[] salary = {"Daily", "Weekly", "Monthly"};
+    public SQLiteDatabase db;
+    public DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,18 +60,46 @@ public class SalaryActivity extends AppCompatActivity {
         SalaryListVIew = (ListView) findViewById(R.id.SalaryListView);
         salaryspinner = (EditText) findViewById(R.id.salarySpinner);
 
+        int oneDay = 1;
+        int oneWeek = 7;
+        int oneMonth = 1;
+
+        Date now = new Date();
+        Date now1 = new Date();
+        Date now2 = new Date();
+
+        Date addday = addDay(now, oneDay);
+        Date addweek = addWeek(now1, oneWeek);
+        Date addmonth = addMonth(now2, oneMonth);
+
+        Log.d(TAG, "add one day: " + addday);
+        Log.d(TAG, "add one week: " + addweek);
+        Log.d(TAG, "add one month: " + addmonth);
+
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        String Daily = df.format(addday);
+        String Weekly = df.format(addweek);
+        String Monthly = df.format(addmonth);
+
+
+
+//        Date [] dates = { addday, addweek, addmonth };
+
+        String[] salary = {Daily, Weekly, Monthly};
+
         ArrayAdapter<String> salarySpinnerAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.support_simple_spinner_dropdown_item,
                 salary
         );
 
-        MaterialBetterSpinner materialLoanSpinner = (MaterialBetterSpinner) findViewById(R.id.salarySpinner);
-        materialLoanSpinner.setAdapter(salarySpinnerAdapter);
+        MaterialBetterSpinner materialSalarySpinner = (MaterialBetterSpinner) findViewById(R.id.salarySpinner);
+        materialSalarySpinner.setAdapter(salarySpinnerAdapter);
 
         incomeCrud = new IncomeCrud(this);
 
         userCrud = new UserCrud(this);
+
 
         ArrayList<Income> loanArrayList = new ArrayList<>();
         loanArrayList = incomeCrud.getAllSalary();
@@ -68,7 +107,27 @@ public class SalaryActivity extends AppCompatActivity {
         salaryAdapter = new SalaryAdapter(this, R.layout.salary_list_adapter, loanArrayList);
         SalaryListVIew.setAdapter(salaryAdapter);
 
+        String currentDate = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+        final String speriod = this.salaryPeriod();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        Date convertedDate = new Date();
+
+        try{
+            convertedDate = dateFormat.parse(speriod);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "converted date: " + convertedDate);
+
+        if (System.currentTimeMillis() > convertedDate.getTime()){
+            btnSalary.setEnabled(true);
+        }
+        else{
+            btnSalary.setEnabled(false);
+        }
         AddSalary();
+        salaryPeriod();
+
     }
 
     public void AddSalary(){
@@ -105,6 +164,35 @@ public class SalaryActivity extends AppCompatActivity {
                 }
         );
 
+
+    }
+
+    public static Date addDay(Date d, int days) {
+       // Calendar cal = Calendar.getInstance().getInstance();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(d);
+        cal.add(Calendar.DAY_OF_YEAR, days);
+        d = cal.getTime();
+        return  d;
+    }
+
+    public static Date addWeek(Date date, int numberOfDays) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_YEAR, numberOfDays);
+        return cal.getTime();
+    }
+
+    public static Date addMonth(Date date, int i) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, i);
+        return cal.getTime();
+    }
+
+    public String salaryPeriod(){
+        String period = incomeCrud.getSalaryPeriod();
+        return period;
 
     }
 

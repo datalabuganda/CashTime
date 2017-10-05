4571c31f02978f6d51a93ce9e7cd774b19401a0e
eq62Roket <otimtony>
2017-10-05 12:54:11 +0300

    Restricted so that the user can only spend on other costs when they have enough income

--- app/src/main/java/com/example/eq62roket/CashTime/activities/OthersActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/OthersActivity.java
@@ -19,6 +19,7 @@ import com.example.eq62roket.CashTime.adapters.EducationAdapter;
 import com.example.eq62roket.CashTime.adapters.OthersAdapter;
 import com.example.eq62roket.CashTime.helper.DatabaseHelper;
 import com.example.eq62roket.CashTime.helper.ExpenditureCrud;
+import com.example.eq62roket.CashTime.helper.IncomeCrud;
 import com.example.eq62roket.CashTime.helper.UserCrud;
 import com.example.eq62roket.CashTime.models.Expenditure;
 import com.example.eq62roket.CashTime.models.User;
@@ -33,6 +34,7 @@ public class OthersActivity extends AppCompatActivity {
     ListView OthersListView;
     UserCrud userCrud;
     OthersAdapter othersAdapter;
+    IncomeCrud incomeCrud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +49,7 @@ public class OthersActivity extends AppCompatActivity {
         OthersListView = (ListView) findViewById(R.id.othersListView);
 
         userCrud = new UserCrud(this);
+        incomeCrud =  new IncomeCrud(this);
 
         ArrayList<Expenditure> othersArrayList = new ArrayList<>();
         othersArrayList = expenditureCrud.getAllOthers();
@@ -55,10 +58,11 @@ public class OthersActivity extends AppCompatActivity {
         OthersListView.setAdapter(othersAdapter);
 
         AddOthers();
-//        populateListView();
+        remainingIncome();
     }
 
     public void AddOthers(){
+        final int remainingincome = this.remainingIncome();
         btnOthers.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -67,21 +71,25 @@ public class OthersActivity extends AppCompatActivity {
 
                         if (!edtOthers.getText().toString().equals("")) {
                             int yVal = Integer.parseInt(String.valueOf(edtOthers.getText()));
-                            boolean isInseted = expenditureCrud.insertOthers(yVal);
-                            if (isInseted) {
-                                // if user spends on any expense, award them 2 points
-                                User user = userCrud.getLastUserInserted();
-                                user.setPoints(2);
-                                user.setSyncStatus(0);
-                                userCrud.updateUser(user);
+                            if (yVal <= remainingincome) {
+                                boolean isInseted = expenditureCrud.insertOthers(yVal);
+                                if (isInseted) {
+                                    // if user spends on any expense, award them 2 points
+                                    User user = userCrud.getLastUserInserted();
+                                    user.setPoints(2);
+                                    user.setSyncStatus(0);
+                                    userCrud.updateUser(user);
 
-                                Intent Othersintent = new Intent(OthersActivity.this, ExpenditureActivity.class);
-                                OthersActivity.this.startActivity(Othersintent);
-                                Toast.makeText(OthersActivity.this, "Your costs of other items have been stored", Toast.LENGTH_LONG).show();
-                                finish();
-                            }
-                            else {
-                                Toast.makeText(OthersActivity.this, "Other costs have not been stored", Toast.LENGTH_LONG).show();
+                                    Intent Othersintent = new Intent(OthersActivity.this, ExpenditureActivity.class);
+                                    OthersActivity.this.startActivity(Othersintent);
+                                    Toast.makeText(OthersActivity.this, "Your costs of other items have been stored", Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(OthersActivity.this, "Other costs have not been stored", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                Toast.makeText(OthersActivity.this, "Your don't have enough income to spend on others", Toast.LENGTH_LONG).show();
+
                             }
                         }
                         else {
@@ -94,51 +102,13 @@ public class OthersActivity extends AppCompatActivity {
 
     }
 
-    private void populateListView(){
-        Log.d(TAG, "populateListView: Displayng data in the listView");
+    public int remainingIncome(){
+        int totalIncome = incomeCrud.addAllIncome();
+        int totalExpenditure = expenditureCrud.addAllCategories();
 
-        Cursor data = expenditureCrud.getOthers();
-        Log.d(TAG, "here is data: " + data);
-//        ArrayList<Income> salaryDataList = new ArrayList<>();
-//        salaryDataList = (ArrayList<Income>) myHelper.getSalary();
-//
-//
-//        SalaryAdapter salaryAdapterListAdapter = new SalaryAdapter(this, R.layout.salary_list_adapter, salaryDataList);
-//        SalaryListVIew.setAdapter(salaryAdapterListAdapter);
+        int remainingIncome = totalIncome - totalExpenditure;
+        return remainingIncome;
 
-        ArrayList<String> listData = new ArrayList<>();
-
-        while (data.moveToNext()){
-            listData.add(data.getString(data.getColumnIndex(DatabaseHelper.COLUMN_EXPENDITURE_OTHERS)));
-
-        }
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-        OthersListView.setAdapter(adapter);
-
-        OthersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String Others = adapterView.getItemAtPosition(i).toString();
-                Log.d(TAG, "onItemClick: You clicked on" + Others);
-
-                Cursor data = expenditureCrud.getOthersID(Others);
-                int OthersID = -1;
-                while (data.moveToNext()){
-                    OthersID = data.getInt(0);
-
-                }
-                if (OthersID > -1){
-                    Intent editOthersIntent = new Intent(OthersActivity.this, UpdateOthersActivity.class);
-                    editOthersIntent.putExtra("id", OthersID);
-                    editOthersIntent.putExtra("Others", Others);
-                    Log.d(TAG, "almost through: " + Others);
-                    startActivity(editOthersIntent);
-                    finish();
-
-                }else {
-
-                }
-            }
-        });
     }
+
 }

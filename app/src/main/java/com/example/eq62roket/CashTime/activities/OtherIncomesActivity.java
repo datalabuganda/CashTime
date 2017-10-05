6c917215c2033e03d3a11b9d3430e99d1c1b24a6
eq62Roket <otimtony>
2017-10-05 12:15:44 +0300

    Created a method to deactivate the button when the current date is less the the period of the amount set by the application user

--- app/src/main/java/com/example/eq62roket/CashTime/activities/OtherIncomesActivity.java
+++ app/src/main/java/com/example/eq62roket/CashTime/activities/OtherIncomesActivity.java
@@ -26,7 +26,13 @@ import com.example.eq62roket.CashTime.models.Income;
 import com.example.eq62roket.CashTime.models.User;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class OtherIncomesActivity extends AppCompatActivity {
 
@@ -52,6 +58,33 @@ public class OtherIncomesActivity extends AppCompatActivity {
 
         othersspinner = (EditText) findViewById(R.id.othersSpinner);
 
+        int oneDay = 1;
+        int oneWeek = 7;
+        int oneMonth = 1;
+
+        Date now = new Date();
+        Date now1 = new Date();
+        Date now2 = new Date();
+
+        Date addday = addDay(now, oneDay);
+        Date addweek = addWeek(now1, oneWeek);
+        Date addmonth = addMonth(now2, oneMonth);
+
+        Log.d(TAG, "add one day: " + addday);
+        Log.d(TAG, "add one week: " + addweek);
+        Log.d(TAG, "add one month: " + addmonth);
+
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        String Daily = df.format(addday);
+        String Weekly = df.format(addweek);
+        String Monthly = df.format(addmonth);
+
+
+
+//        Date [] dates = { addday, addweek, addmonth };
+
+        String[] others = {Daily, Weekly, Monthly};
+
         // adapter for gender
         ArrayAdapter<String> othersSpinnerAdapter = new ArrayAdapter<String>(
                 this,
@@ -66,6 +99,26 @@ public class OtherIncomesActivity extends AppCompatActivity {
 
         userCrud = new UserCrud(this);
 
+        String currentDate = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+        final String speriod = this.othersPeriod();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        Date convertedDate = new Date();
+
+        try{
+            convertedDate = dateFormat.parse(speriod);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "converted date: " + convertedDate);
+
+        if (System.currentTimeMillis() > convertedDate.getTime()){
+            btnOthers.setEnabled(true);
+        }
+        else{
+            btnOthers.setEnabled(false);
+        }
+
+
         ArrayList<Income> loanArrayList = new ArrayList<>();
         loanArrayList = incomeCrud.getAllOthers();
 
@@ -73,7 +126,7 @@ public class OtherIncomesActivity extends AppCompatActivity {
         othersListVIew.setAdapter(otherincomeAdapter);
 
 
-
+        othersPeriod();
         AddOthers();
     }
 
@@ -109,6 +162,35 @@ public class OtherIncomesActivity extends AppCompatActivity {
 
                 }
         );
+    }
+
+    public static Date addDay(Date d, int days) {
+        // Calendar cal = Calendar.getInstance().getInstance();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(d);
+        cal.add(Calendar.DAY_OF_YEAR, days);
+        d = cal.getTime();
+        return  d;
+    }
+
+    public static Date addWeek(Date date, int numberOfDays) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_YEAR, numberOfDays);
+        return cal.getTime();
+    }
+
+    public static Date addMonth(Date date, int i) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, i);
+        return cal.getTime();
+    }
+
+    public String othersPeriod(){
+        String period = incomeCrud.getOthersPeriod();
+        return period;
+
     }
 
 

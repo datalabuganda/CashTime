0630793db75905ba9263543ca7b0a810623ba33a
OtimTony <otimtonyjeff@gmail.com>
2018-03-01 22:21:09 +0300

    TabbedAnalysisActivity to display group analysis fragments with summary of group goals its transactions and member analysis fragment to display a summary of individual transactions

--- app/src/main/java/com/example/eq62roket/cashtime/MembersAnalysisFragment.java
+++ app/src/main/java/com/example/eq62roket/cashtime/MembersAnalysisFragment.java
@@ -1,6 +1,7 @@
 package com.example.eq62roket.cashtime;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,101 +9,99 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MembersAnalysisFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MembersAnalysisFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+import java.util.ArrayList;
+
 public class MembersAnalysisFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MembersAnalysisFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MembersAnalysisFragment newInstance(String param1, String param2) {
-        MembersAnalysisFragment fragment = new MembersAnalysisFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-    public MembersAnalysisFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    PieChart membersPieChart;
+    BarChart membersBarChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_members_analysis, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_members_analysis, container, false);
+
+        membersPieChart = (PieChart) rootView.findViewById(R.id.membersPieChart);
+        membersBarChart = (BarChart) rootView.findViewById(R.id.membersBarGraph);
+
+
+        membersPieChart.setDragDecelerationFrictionCoef(0.99f);
+
+        membersPieChart.setDrawHoleEnabled(false);
+        membersPieChart.setHoleColor(Color.WHITE);
+        membersPieChart.setDrawEntryLabels(false);
+        membersPieChart.setTransparentCircleRadius(1f);
+//        summaryPieChart.setCenterText("Expenses");
+//        summaryPieChart.setCenterTextColor(Color.RED);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        yValues.add(new PieEntry(31f, "Rik"));
+        yValues.add(new PieEntry(24f, "Tony"));
+        yValues.add(new PieEntry(55f, "Sytskey"));
+        yValues.add(new PieEntry(45f, "Ivan"));
+        yValues.add(new PieEntry(67f, "Patricia"));
+        yValues.add(new PieEntry(10f, "Probuse"));
+
+
+        membersPieChart.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        PieDataSet dataSet = new PieDataSet(yValues, "");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+
+        membersPieChart.setData(data);
+
+        //Expense BarChart
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        entries.add(new BarEntry(1, 20));
+        entries.add(new BarEntry(2, 34));
+        entries.add(new BarEntry(3, 22));
+        entries.add(new BarEntry(4, 55));
+        entries.add(new BarEntry(5, 14));
+
+        BarDataSet barDataSet = new BarDataSet(entries, "Amount Saved");
+
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Otim");
+        labels.add("Rik");
+        labels.add("Ivan");
+        labels.add("Patricia");
+        labels.add("Probuse");
+
+        membersBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+
+        BarData barData = new BarData(barDataSet);
+
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        membersBarChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+//        summartBarChart.getXAxis().setLabelsToSkip(0);
+        membersBarChart.setTouchEnabled(false);
+        membersBarChart.setDragEnabled(false);
+        membersBarChart.setScaleEnabled(false);
+        membersBarChart.setVisibleXRangeMaximum(1);
+        membersBarChart.setData(barData);
+
+        return rootView;
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }

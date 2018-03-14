cad5dbedce660cc955d8d3deda85a0208b1ec7af
OtimTony <otimtonyjeff@gmail.com>
2018-03-14 18:34:46 +0300

    Created profileActivity and edit user profile activity

--- app/src/main/java/com/example/eq62roket/cashtime/ProfileActivity.java
+++ app/src/main/java/com/example/eq62roket/cashtime/ProfileActivity.java
@@ -1,13 +1,26 @@
 package com.example.eq62roket.cashtime;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView editProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        editProfile = (TextView)findViewById(R.id.editProfile);
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent editProfileIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(editProfileIntent);
+            }
+        });
     }
 }

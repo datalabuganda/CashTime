6d6546c29bbc9fc7c93ce9f00f3649a255403bd0
OtimTony <otimtonyjeff@gmail.com>
2018-03-14 18:42:33 +0300

    Onclick interface for a single groupmember

--- app/src/main/java/com/example/eq62roket/cashtime/Interfaces/ItemClickListener.java
+++ app/src/main/java/com/example/eq62roket/cashtime/Interfaces/ItemClickListener.java
@@ -1,8 +1,11 @@
 package com.example.eq62roket.cashtime.Interfaces;
 
+import android.view.View;
+
 /**
  * Created by eq62roket on 3/12/18.
  */
 
 public interface ItemClickListener {
+    void onItemClick(View v, int pos);
 }

d33a4fadff5e33d12c1986dac54f10126467fe5b
OtimTony <otimtonyjeff@gmail.com>
2018-03-23 16:22:33 +0300

    Initialised parse

--- app/src/main/java/com/example/eq62roket/cashtime/RegistrationActivity.java
+++ app/src/main/java/com/example/eq62roket/cashtime/RegistrationActivity.java
@@ -3,11 +3,20 @@ package com.example.eq62roket.cashtime;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.ParseObject;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        Parse.initialize(this);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }

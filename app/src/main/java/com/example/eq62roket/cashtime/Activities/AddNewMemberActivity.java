fb956ec2f03902eca26de99ed20ae65936d26f76
probuse <nprobuse@gmail.com>
2018-03-25 08:53:35 +0300

    Added array of levels of education to add new member activity

--- app/src/main/java/com/example/eq62roket/cashtime/Activities/AddNewMemberActivity.java
+++ app/src/main/java/com/example/eq62roket/cashtime/Activities/AddNewMemberActivity.java
@@ -1,35 +1,29 @@
 package com.example.eq62roket.cashtime.Activities;
 
-import android.support.annotation.MainThread;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.eq62roket.cashtime.Helper.DatabaseHelper;
-import com.example.eq62roket.cashtime.adapters.DatabaseAdapter;
 
 import com.example.eq62roket.cashtime.R;
 
 public class AddNewMemberActivity extends AppCompatActivity {
-    DatabaseHelper myDatabaseHelper;
-    EditText editName, editPhoneNumber, editNationality, editHouseHoldComposition, editLocation, editLevelOfEducation;
+    EditText editName, editPhoneNumber, editNationality, editLocation, editLevelOfEducation;
     TextView addNewMember;
+    Spinner  houseHoldComposition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_member);
-        myDatabaseHelper = new DatabaseHelper(this);
 
         editName = (EditText)findViewById(R.id.memberName);
         editPhoneNumber = (EditText)findViewById(R.id.memberPhoneNumber);
         editNationality = (EditText)findViewById(R.id.memberNationality);
-        editHouseHoldComposition = (EditText)findViewById(R.id.memberHouseholdCompostion);
+        houseHoldComposition = (Spinner)findViewById(R.id.memberHouseholdCompostion);
         editLocation = (EditText)findViewById(R.id.memberLocation);
         editLevelOfEducation= (EditText)findViewById(R.id.memberLevelOfEducation);
 
@@ -45,23 +39,8 @@ public class AddNewMemberActivity extends AppCompatActivity {
 
     }
 
-    private void addNewGroupMember(String name, String phone){
-        DatabaseAdapter databaseAdapter = new DatabaseAdapter(this);
-
-        databaseAdapter.openDatabase();
-
-        long result = databaseAdapter.addNewGroupMember(name,phone);
-
-        if (result>0){
-            editName.setText("");
-            editPhoneNumber.setText("");
-            Toast.makeText(AddNewMemberActivity.this,"New member added", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(AddNewMemberActivity.this, "Failed to add new member", Toast.LENGTH_SHORT).show();
-
-        }
-
-        databaseAdapter.close();
+    private void addNewGroupMember(String name, String phone) {
+        Toast.makeText(this, "Adding a new Member.....", Toast.LENGTH_SHORT).show();
     }
 
 }
